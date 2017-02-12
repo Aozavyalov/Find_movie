28125e7d2be5d1e51cc281615a8cbfd39cfefcaa
Andrew <Aozavyalov@protonmail.com>
2017-02-12 13:18:54 +0300

    Added searching by original title in 3 task

--- Third_task.py
+++ Third_task.py
@@ -7,5 +7,5 @@ for str_json in in_file:
 print('Введите слово, которое содержится в названии фильма:')
 name = input()
 for film in films:
-    if film['title'].lower().find(name.lower()) != -1:
+    if film['title'].lower().find(name.lower()) != -1 or film['original_title'].lower().find(name.lower()) != -1:
         print(film['title'])
