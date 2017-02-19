8ddbf81c97a58d8d556518fd48a92c87f879958d
Andrew <Aozavyalov@protonmail.com>
2017-02-19 19:53:59 +0300

    Don't know why, it's didn't commit

--- Download_1000_films_to_json.py
+++ Download_1000_films_to_json.py
@@ -1,68 +1,71 @@
 import urllib.request
 import urllib.parse
+import urllib.error
 import json
 import time
 
 
+def write_films_to_file(films_list, filename):
+    with open(filename, 'w') as file:
+        for film in films_list:
+            file.write(json.dumps(film) + '\n')
+
+
 def try_to_get_film(film_number, api_key):
-    films_file = open("films.json", 'a')
-    url = 'https://api.themoviedb.org/3/movie/%s?api_key=%s&language=ru' % (str(film_number), api_key)
+    url = 'https://api.themoviedb.org/3/movie/%d?api_key=%s&language=ru' % (film_number, api_key)
     try:
         film = urllib.request.urlopen(url).read().decode('utf-8')
-        films_file.write(film + '\n')
-        films_file.close()
         print('Film number %d saved' % film_number)
         return json.loads(film)
-    except:
+    except urllib.error.HTTPError:
         print('Can`t save film #%d' % film_number)
-        films_file.close()
         return {}
 
 
-def sleep_if_no_10_secs(time_of_cycle, num):    # sleep, if too much requests in 10 secs
-    if time.time() - time_of_cycle < 10 and num % 40 == 0:
+def sleep_if_too_much_requests(time_of_cycle, max_time_of_requests, count_of_requests, max_requests_in_time):
+    if time.time() - time_of_cycle < max_time_of_requests and count_of_requests % max_requests_in_time == 0:
         time.sleep(10 - (time.time() - time_of_cycle))
-    return time.time()
+        return time.time()
+    else:
+        return time_of_cycle
 
 
-def file_check():
+def file_check(filename):
     try:
-        films_file = open("films.json", 'r')
         len_of_file = 0
-        for line in films_file:
-            len_of_file += 1
-        films_file.close()
-        if len_of_file >= 1000:
-            return True
-        else:
-            return False
-    except:
+        with open(filename, 'r') as films_file:
+            for line in films_file:
+                len_of_file += 1
+        return len_of_file >= 1000
+    except FileNotFoundError:
         return False
 
 
-def write_films_from_file_to_list(films):
-    films_file = open("films.json", 'r')
-    for film in films_file:
-        films.append(json.loads(film))
-    films_file.close()
+def write_films_from_file_to_list(films, filename):
+    with open(filename, 'r') as films_file:
+        for film in films_file:
+            films.append(json.loads(film))
 
 
 def get_1000_films():
     films_base = []
-    if not file_check():
+    file = 'films.json'
+    if not file_check(file):
         print('Input your api key:')
         api_key = input()
         time_of_cycle = time.time()
         film_number = 1
         while len(films_base) != 1000:
-            time_of_cycle = sleep_if_no_10_secs(time_of_cycle=time_of_cycle, num=film_number)
+            time_of_cycle = sleep_if_too_much_requests(time_of_cycle, 10, film_number, 40)
             film = try_to_get_film(film_number, api_key)
             if film != {}:
                 films_base.append(film)
             film_number += 1
+        write_films_to_file(films_base, file)
     else:
-        write_films_from_file_to_list(films=films_base)
-        print('There are %d films in file' % len(films_base))
+        write_films_from_file_to_list(films_base, file)
+        if __name__ == "__main__":
+            print('There are %d films in file' % len(films_base))
     return films_base
 
 
--- Find_films_by_name.py
+++ Find_films_by_name.py
@@ -1,14 +1,24 @@
-import Download_1000_films_to_json
+import download_1000_films_to_json
 
 
-def find_film_by_name():
-    films = Download_1000_films_to_json.get_1000_films()
-    print('Input a word from a film`s title:')
-    name = input()
-    for film in films:
-        if film['title'].lower().find(name.lower()) != -1 or film['original_title'].lower().find(name.lower()) != -1:
+def find_films_by_name(film_title, all_films):
+    founded_films = []
+    for film in all_films:
+        if film['title'].lower().find(film_title.lower()) != -1 or film['original_title'].lower().find(film_title.lower()) != -1:
+            founded_films.append(film)
+    return founded_films
+
+
+def print_founded_films(founded_films, film_title):
+    for film in founded_films:
+        if film_title.lower() == film['title'].lower():
+            print('There is film with the same title: %s )' % film['title'])
+        else:
             print(film['title'])
 
-
 if __name__ == '__main__':
-    find_film_by_name()
+    films = download_1000_films_to_json.get_1000_films()
+    print('Input a word from a film`s title:')
+    name = input()
+    films = find_films_by_name(name, films)
+    print_founded_films(films, name)
